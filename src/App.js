a08e1bd17970527220a94499a8f6ea119f7466f3
nidhinbabu44 <nidhinbabu44@gmail.com>
2022-05-13 15:50:33 +0530

    react register

--- src/App.js
+++ src/App.js
@@ -6,26 +6,52 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 function App() {
   return (
     <div className="container">
-    <div className="row">
-        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-<div className="row g-3">
-<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-<label for="" className="form">User Name</label>
-<input placeholder='Enter your User Name' type="text" className="form-control"/>
-</div>
-<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-    <label for="" className="form"> Password</label>
-    <input placeholder='Enter your Password' type="password" className="form-control"/>
-</div>
-<div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-<button className="btn btn-success">Login</button>
-</div>
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+<label for="" className="form-label">Name</label>
+<input placeholder="Enter your name" type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
-</div>
+                        <label for="" className="form-label">Roll Number</label>
+                        <input placeholder="Enter your Roll Number" type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Admission Number</label>
+                        <input placeholder="Enter your Admission Number" type="text" className="form-control"/>
 
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Mobile Number</label>
+                        <input placeholder="Enter your Mobile Number" type="text" className="form-control"/>
+
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label for="" className="form-label">College</label>
+                        <input placeholder="Enter your College" type="text" className="form-control"/>
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">User Name</label>
+                        <input placeholder="Enter your User Name" type="text" className="form-control"/>
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Password</label>
+                        <input placeholder="Enter your Password"  type="password" className="form-control"/>
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <button className="btn btn-warning">Register</button>
+
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     </div>
-</div>
   );
 }
 
